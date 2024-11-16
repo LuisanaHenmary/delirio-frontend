1fb6ecf24e0df991d7013b21e0ace707e20f6a49
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-11-16 16:52:54 -0400

    Add providers

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,27 @@ import App from './App.jsx'
 import { AuthContextProvider } from './contexts/authContext.jsx'
 import { StatusContextProvider } from './contexts/statusContext.jsx'
 import { ToDoContextProvider } from './contexts/toDoContext.jsx'
+import { JobContextProvider } from './contexts/jobsContext'
+import { ProjectsContextProvider } from './contexts/projectsContext'
+import { EmployersContextProvider } from './contexts/employersContext'
+import { CompaniesContextProvider } from './contexts/companiesContext'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContextProvider>
       <StatusContextProvider>
-        <ToDoContextProvider>
-          <App />
-        </ToDoContextProvider>
+        <JobContextProvider>
+          <ProjectsContextProvider>
+            <EmployersContextProvider>
+              <CompaniesContextProvider>
+                <ToDoContextProvider>
+                  <App />
+                </ToDoContextProvider>
+              </CompaniesContextProvider>
+            </EmployersContextProvider>
+          </ProjectsContextProvider>
+        </JobContextProvider>
       </StatusContextProvider>
     </AuthContextProvider>
   </StrictMode>,
