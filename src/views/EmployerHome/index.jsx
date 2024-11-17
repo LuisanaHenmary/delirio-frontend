ec73a9c1067d9fe69f2d3ce91fcf700b4171eeaa
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-11-17 15:42:59 -0400

    fixe the employer home

--- src/views/EmployerHome/index.jsx
+++ src/views/EmployerHome/index.jsx
@@ -1,28 +1,13 @@
 import CalendarView from "../Calendar"
 import { useToDoContext } from "../../hooks/useToDoContext";
-import { useAuthContext } from '../../hooks/useAuthContext';
-import { useState, useEffect } from "react";
 
-const EmployerHome = ({id_employer}) =>{
-
+const EmployerHome = () => {
     const { todoes } = useToDoContext()
-    const [events, setEvents] = useState([])
 
-    useEffect(()=>{
-
-        const toDoesEmployer = todoes.filter((value)=>{
-            return value.data.id_employer == id_employer
-        })
-
-        setEvents(toDoesEmployer)
-
-    },[id_employer, todoes])
-
-
-    return(
+    return (
         <>
-        Welcome empleado
-        <CalendarView events={events} />
+            Welcome empleado
+            <CalendarView events={todoes} />
         </>
     )
 }
