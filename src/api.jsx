316f066f73f38ea91f52d0859da6aa2a940fdb74
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-11-17 15:40:12 -0400

    To-does per user type

--- src/api.jsx
+++ src/api.jsx
@@ -4,36 +4,101 @@ export const getToDoes = async (user, dispatch) => {
 
     const apiUrl = import.meta.env.VITE_API_URL
 
+    if (user.role == "admin") {
+        const response = await axios({
+            method: 'get',
+            url: `${apiUrl}/to-does`,
+            headers: {
+                'Authorization': `Bearer ${user.token}`,
+            },
+        });
 
-    const response = await axios({
-        method: 'get',
-        url: `${apiUrl}/to-does`,
-        headers: {
-            'Authorization': `Bearer ${user.token}`,
-        },
-    });
-
-    const data = await response.data
-
-    const events = data.map((event) => {
-        const { id_todo, title, expired, id_status, id_employer, id_company } = event
-
-        return {
-            'title': title,
-            'start': new Date(expired),
-            'end': new Date(expired),
-            'data': {
-                'id': id_todo,
+        const data = await response.data
+    
+        const events = data.map((event) => {
+            const { id_todo, title, expired, id_status, id_employer, id_company } = event
+    
+            return {
                 'title': title,
-                'expired': expired,
-                'id_status': id_status,
-                'id_employer': id_employer,
-                "id_company": id_company
+                'start': new Date(expired),
+                'end': new Date(expired),
+                'data': {
+                    'id': id_todo,
+                    'title': title,
+                    'expired': expired,
+                    'id_status': id_status,
+                    'id_employer': id_employer,
+                    "id_company": id_company
+                }
             }
-        }
-    })
+        })
+    
+        dispatch({ type: 'SET_TO_DOES', payload: events })
 
-    dispatch({ type: 'SET_TO_DOES', payload: events })
+    }else if(user.role == "employer"){
+        const response = await axios({
+            method: 'get',
+            url: `${apiUrl}/to-does/employer/${user.id_user}`,
+            headers: {
+                'Authorization': `Bearer ${user.token}`,
+            },
+        });
+
+        const data = await response.data
+    
+        const events = data.map((event) => {
+            const { id_todo, title, expired, id_status, id_employer, id_company } = event
+    
+            return {
+                'title': title,
+                'start': new Date(expired),
+                'end': new Date(expired),
+                'data': {
+                    'id': id_todo,
+                    'title': title,
+                    'expired': expired,
+                    'id_status': id_status,
+                    'id_employer': id_employer,
+                    "id_company": id_company
+                }
+            }
+        })
+    
+        dispatch({ type: 'SET_TO_DOES', payload: events })
+
+    }else if(user.role == "company"){
+        const response = await axios({
+            method: 'get',
+            url: `${apiUrl}/to-does/company/${user.id_user}`,
+            headers: {
+                'Authorization': `Bearer ${user.token}`,
+            },
+        });
+
+        const data = await response.data
+    
+        const events = data.map((event) => {
+            const { id_todo, title, expired, id_status, id_employer, id_company } = event
+    
+            return {
+                'title': title,
+                'start': new Date(expired),
+                'end': new Date(expired),
+                'data': {
+                    'id': id_todo,
+                    'title': title,
+                    'expired': expired,
+                    'id_status': id_status,
+                    'id_employer': id_employer,
+                    "id_company": id_company
+                }
+            }
+        })
+    
+        dispatch({ type: 'SET_TO_DOES', payload: events })
+    }
+    
+    
 }
 
 export const getEmployers = async (user, dispatchEmployers) => {
