12b1822b3c486b2040a9b5ecc45855c7b1050caf
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-11-21 15:34:31 -0400

    Implement ToDoCarAdmin

--- src/views/AdminHome/index.jsx
+++ src/views/AdminHome/index.jsx
@@ -2,17 +2,68 @@ import CalendarView from "../Calendar"
 import {
     Box, Typography
 } from "@mui/material";
+import { useState, use } from "react";
 import { useToDoContext } from "../../hooks/useToDoContext";
 import TableEmployers from "../../components/TableEmployers";
 import TableCompanies from "../../components/TableCompanies";
+import { useCompaniesContext } from "../../hooks/useCompanyContext";
+import { useStatusContext } from "../../hooks/useStatusContext";
+import { useEmployersContext } from "../../hooks/useEmployersContext";
+import { useOpen } from "../../hooks/useOpen";
+import ToDoCardAdmin from "../../components/ToDoCardAdmin";
+import dayjs from 'dayjs';
 
 
 const AdminHome = () => {
     const { todoes } = useToDoContext()
+    const { companies } = useCompaniesContext()
+    const { statues } = useStatusContext()
+    const { employers } = useEmployersContext()
+    const [open, changeToOpen, changeToClose] = useOpen()
+    const [toDoSelected, setToDoSelected] = useState({
+        'id': 1,
+        'title': "",
+        'expired': dayjs(),
+        'statusName': "",
+        'employerIndex': 0,
+        "companyNamey": "",
+        "statusClass": ""
+    })
 
     const clickTodo = (e) => {
+        const expired = e.event._instance.range.start
         const data = e.event._def.extendedProps.data
-        console.log(data)
+        const { id, title, id_status, id_company, id_employer } = data
+
+        const company = companies.filter((value) => {
+            return parseInt(value.id_company) == parseInt(id_company)
+        })
+
+        const companyName = company[0]['name_company']
+
+        const status = statues.filter((value) => {
+            return parseInt(value.id_status) == parseInt(id_status)
+        })
+
+        const employerIndex = employers.findIndex(employer => 
+            parseInt(employer.id_employer) == parseInt(id_employer)
+        );
+
+        const statusName = status[0]['name_status']
+        const statusClass = status[0]['className']
+
+        const info = {
+            id,
+            title,
+            expired,
+            statusName,
+            companyName,
+            statusClass,
+            employerIndex
+        }
+
+        setToDoSelected(info)
+        changeToOpen()
     }
 
     return (
@@ -24,7 +75,7 @@ const AdminHome = () => {
                 <TableEmployers />
                 <TableCompanies />
             </Box>
-
+            <ToDoCardAdmin open={open} info={toDoSelected} handleClose={changeToClose} />
         </>
     )
 }
