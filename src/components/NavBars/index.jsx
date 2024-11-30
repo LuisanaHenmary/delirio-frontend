4b6c2c17b12ac8e2d3df9353a97d9816416a13f2
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-11-30 15:19:48 -0400

    Fixed Navbar

--- src/components/NavBars/index.jsx
+++ src/components/NavBars/index.jsx
@@ -1,10 +1,11 @@
 import {
     Toolbar,
     Typography,
-    IconButton
+    IconButton,
+    Box
 } from '@mui/material';
 import MuiAppBar from '@mui/material/AppBar';
-import MenuIcon from '@mui/icons-material/Menu';
+import SensorOccupiedIcon from '@mui/icons-material/SensorOccupied';
 import { useOpen } from '../../hooks/useOpen';
 import "./index.css"
 
@@ -12,8 +13,10 @@ import { useLogout, useClear } from "../../hooks/useLogout"
 import DrawerMenu from '../DrawerMenu';
 import AddProfile from "../../views/AddProfile";
 
+import { HideOnScroll } from '../HideOnScroll';
 
-const NavBar = () => {
+
+const NavBar = ({ username }) => {
     const { logout } = useLogout()
     const { clearLists } = useClear()
     const [openMenu, changeToOpenMenu, changeToCloseMenu] = useOpen()
@@ -26,29 +29,43 @@ const NavBar = () => {
     }
 
     return (
-        <>
-            <DrawerMenu openMenu={openMenu} changeToCloseMenu={changeToCloseMenu} handleClick={handleClick} changeToOpenAddProfile={changeToOpenAddProfile} />
+        <Box  component="div" marginBottom={"100px"} >
+            <DrawerMenu
+                openMenu={openMenu}
+                changeToCloseMenu={changeToCloseMenu}
+                handleClick={handleClick}
+                changeToOpenAddProfile={changeToOpenAddProfile}
+            />
 
-           
-                <MuiAppBar sx={{ backgroundColor: 'rgba(240, 248, 255, 0)', boxShadow: 0 }} >
+
+            <HideOnScroll>
+                <MuiAppBar sx={{ backgroundColor: "rgba(255, 255, 255, 0);", boxShadow: 0 }} >
                     <Toolbar>
                         <Typography variant="h6" noWrap sx={{ flexGrow: 1, color: 'blue' }} component="div">
                             Delirio
                         </Typography>
-                        <IconButton
-                            color="inherit"
-                            aria-label="open drawer"
-                            edge="end"
-                            onClick={changeToOpenMenu}
-                            sx={{ color: 'black', padding: '10px' }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
+
+                        <div>
+                            <Typography variant="h6" noWrap sx={{ flexGrow: 1, color: 'black', paddingRight: "5px" }} component="span">
+                                Hola, {username}
+                            </Typography>
+                            <IconButton
+
+                                color="inherit"
+                                aria-label="open drawer"
+                                edge="end"
+                                onClick={changeToOpenMenu}
+                                sx={{ color: 'white', padding: '10px', background: "linear-gradient(#3DA2DB, #006096)" }}
+                            >
+                                <SensorOccupiedIcon />
+                            </IconButton>
+                        </div>
                     </Toolbar>
                 </MuiAppBar>
+            </HideOnScroll>
 
             <AddProfile open={addProfile} handleClose={changeToCloseAddProfile} />
-        </>
+        </Box>
 
     )
 }
